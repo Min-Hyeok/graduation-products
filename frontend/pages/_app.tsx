aa744ef6a0c44d0f7550126a3395ce7ff572098d
Minhyeok <alsgur0008@naver.com>
2022-09-29 19:35:10 +0900

    alias 설정 추가

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,7 +1,7 @@
 import type { AppProps } from 'next/app';
 import { ThemeProvider } from 'styled-components';
-import theme from '../styles/theme';
-import GlobalStyle from '../styles/GlobalStyle';
+import theme from '@styles/theme';
+import GlobalStyle from '@styles/GlobalStyle';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
