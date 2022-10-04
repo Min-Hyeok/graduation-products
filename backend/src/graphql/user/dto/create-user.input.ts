216e3b6f7f9de8394fdddc16195d5a32c77f8193
minhyeok <alsgur0008@naver.com>
2022-10-04 16:29:43 +0900

    비밀번호 암호화 처리

--- backend/src/graphql/user/dto/create-user.input.ts
+++ backend/src/graphql/user/dto/create-user.input.ts
@@ -1,27 +1,38 @@
 import { Field, InputType } from '@nestjs/graphql';
-import { IsEmail, IsNotEmpty, IsPhoneNumber } from 'class-validator';
+import { IsEmail, IsNotEmpty, IsPhoneNumber, Length } from 'class-validator';
 
 @InputType()
 export class CreateUserInput {
   @Field(() => String, { description: '아이디' })
-  @IsNotEmpty()
-  userId: string;
+  @IsNotEmpty({ message: '아이디는 필수입력 값입니다.' })
+  @Length(4, 20, {
+    message: '아이디는 4글자 이상 20글자 미만으로 입력해 주세요.',
+  })
+  userId = '';
 
   @Field(() => String, { description: '비밀번호' })
-  @IsNotEmpty()
-  password: string;
+  @IsNotEmpty({ message: '비밀번호는 필수입력 값입니다.' })
+  @Length(8, 30, {
+    message: '비밀번호는 최소 8글자 이상 30글자 미만으로 입력해야 합니다.',
+  })
+  password = '';
 
   @Field(() => String, { description: '이름(닉네임)' })
-  @IsNotEmpty()
-  userName: string;
+  @IsNotEmpty({ message: '이름(닉네임)은 필수입력 값입니다.' })
+  @Length(1, 12, {
+    message: '이름은 1글자 이상 12글자 미만으로 입력해야 합니다.',
+  })
+  userName = '';
 
   @Field(() => String, { description: '휴대전화 번호' })
-  @IsPhoneNumber()
-  @IsNotEmpty()
-  phoneNumber: string;
+  @IsNotEmpty({ message: '전화번호는 필수입력 값입니다.' })
+  @IsPhoneNumber('KR', { message: '전화번호 양식이 올바르지 않습니다.' })
+  @Length(8, 20, { message: '전화번호 길이가 잘못되었습니다.' })
+  phoneNumber = '';
 
   @Field(() => String, { description: '이메일' })
-  @IsEmail()
-  @IsNotEmpty()
-  email: string;
+  @IsNotEmpty({ message: '이메일 주소는 필수입력 값입니다.' })
+  @IsEmail({}, { message: '이메일 주소 양식이 올바르지 않습니다.' })
+  @Length(5, 50, { message: '이메일 주소 길이가 올바르지 않습니다.' })
+  email = '';
 }
--- backend/src/graphql/user/user.service.ts
+++ backend/src/graphql/user/user.service.ts
@@ -3,13 +3,16 @@ import { CreateUserInput } from './dto/create-user.input';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
 import { Repository } from 'typeorm';
-import { UpdateUserInput } from '@graphql/user/dto/update-user.input';
+import { UpdateUserInput } from './dto/update-user.input';
+import * as bcrypt from 'bcrypt';
+
+const saltRounds = 10;
 
 @Injectable()
 export class UserService {
   constructor(
     @InjectRepository(User)
-    private userRepository: Repository<User>,
+    private readonly userRepository: Repository<User>,
   ) {}
 
   findUserAll(): Promise<User[]> {
@@ -17,11 +20,23 @@ export class UserService {
   }
 
   async signUp(createUserInput: CreateUserInput): Promise<UpdateUserInput> {
+    const encryptedPassword = await bcrypt.hash(
+      createUserInput.password,
+      saltRounds,
+    );
+
     await this.userRepository
       .createQueryBuilder()
       .insert()
       .into(User)
-      .values([createUserInput])
+      .values([
+        {
+          ...createUserInput,
+          password: encryptedPassword,
+          registerDate: new Date(),
+          loginDate: new Date(),
+        },
+      ])
       .execute();
 
     return {
